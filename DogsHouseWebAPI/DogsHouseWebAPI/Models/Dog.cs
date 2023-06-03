2f1164e7f0de079918eb47666332394093bdf66c
Dersoo <deroo@dersoo>
2023-06-03 14:24:40 +0300

    Add Dog model

---
+++ DogsHouseWebAPI/DogsHouseWebAPI/Models/Dog.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DogsHouseWebAPI.Models
+{
+    public class Dog
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        [Required]
+        public string Name { get; set; } = String.Empty;
+
+        [StringLength(20)]
+        [Required]
+        public string Color { get; set; } = String.Empty;
+
+        [Required]
+        public ushort TailLength { get; set; }
+
+        [Required]
+        public ushort Weight { get; set; }
+
+        [Timestamp]
+        public byte[]? Timestamp { get; set; }
+    }
+}
