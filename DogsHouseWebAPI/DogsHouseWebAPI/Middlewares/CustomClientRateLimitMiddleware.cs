a2ffe271f54d0a3ddfbe23e48d04a5060a15e827
Dersoo <deroo@dersoo>
2023-06-05 00:00:37 +0300

    Add rate limiting

---
+++ DogsHouseWebAPI/DogsHouseWebAPI/Middlewares/CustomClientRateLimitMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using AspNetCoreRateLimit;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
+
+namespace DogsHouseWebAPI.Middlewares
+{
+    public class CustomClientRateLimitMiddleware : ClientRateLimitMiddleware
+    {
+        public CustomClientRateLimitMiddleware(RequestDelegate next,
+            IProcessingStrategy processingStrategy,
+            IOptions<ClientRateLimitOptions> options,
+            IClientPolicyStore policyStore,
+            IRateLimitConfiguration config,
+            ILogger<ClientRateLimitMiddleware> logger) :
+                base(next, processingStrategy, options, policyStore, config, logger)
+        {
+
+        }
+
+        public override Task ReturnQuotaExceededResponse(HttpContext httpContext, RateLimitRule rule, string retryAfter)
+        {
+            string? path = httpContext?.Request?.Path.Value;
+            var result = JsonSerializer.Serialize("API calls quota exceeded!");
+            httpContext.Response.Headers["Retry-After"] = retryAfter;
+            httpContext.Response.StatusCode = 429;
+            httpContext.Response.ContentType = "application/json";
+
+            return httpContext.Response.WriteAsync(result);
+        }
+    }
+}
--- DogsHouseWebAPI/DogsHouseWebAPI/Program.cs
+++ DogsHouseWebAPI/DogsHouseWebAPI/Program.cs
@@ -1,4 +1,6 @@
+using AspNetCoreRateLimit;
 using Contracts;
+using DogsHouseWebAPI.Middlewares;
 using Entities.EF;
 using Entities.Helpers.SortHelper;
 using Entities.Models;
@@ -26,6 +28,29 @@ namespace DogsHouseWebAPI
             builder.Services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
             builder.Services.AddScoped<ISortHelper<Dog>, SortHelper<Dog>>();
 
+            builder.Services.AddMemoryCache();
+            builder.Services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+            builder.Services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+            builder.Services.AddSingleton<IProcessingStrategy, AsyncKeyLockProcessingStrategy>();
+            builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+            builder.Services.AddInMemoryRateLimiting();
+            builder.Services.Configure<ClientRateLimitOptions>(options =>
+            {
+                options.EnableEndpointRateLimiting = true;
+                options.StackBlockedRequests = false;
+                options.HttpStatusCode = 429;
+                options.ClientIdHeader = "X-ClientId";
+                options.GeneralRules = new List<RateLimitRule>
+                {
+                    new RateLimitRule
+                    {
+                        Endpoint = "*",
+                        Period = "1s",
+                        Limit = 10
+                    }
+                };
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -35,11 +60,11 @@ namespace DogsHouseWebAPI
                 app.UseSwaggerUI();
             }
 
+            app.UseMiddleware<CustomClientRateLimitMiddleware>();
+
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
-
             app.MapControllers();
 
             app.Run();
