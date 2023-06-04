bc1ca63d41c914d55c4379c98eb15af2bb2732ba
Dersoo <deroo@dersoo>
2023-06-05 00:30:27 +0300

    Add Ping controller

---
+++ DogsHouseWebAPI/DogsHouseWebAPI/Controllers/PingController.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace DogsHouseWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        public PingController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Dogs house service. Version 1.0.1");
+        }
+    }
+}
